d63bee24f848d741f808c307271f1ef4fd339b74
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-01-26 16:51:45 +0200

    update data

--- part3_phonebook_backend/index.js
+++ part3_phonebook_backend/index.js
@@ -29,22 +29,26 @@ let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
-      "number": "040-123456"
+      "number": "040-123456",
+      "toShow": true
     },
     { 
       "id": 2,
       "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      "number": "39-44-5323523",
+      "toShow": true
     },
     { 
       "id": 3,
       "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      "number": "12-43-234345",
+      "toShow": true
     },
     { 
       "id": 4,
       "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      "number": "39-23-6423122",
+      "toShow": true
     }
 ]
 
