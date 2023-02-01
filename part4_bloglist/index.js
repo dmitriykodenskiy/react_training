9410b4d1be08eba03fda05a58fec8fd0bc720940
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-02-01 16:01:08 +0200

    redo initial app

--- part4_bloglist/index.js
+++ part4_bloglist/index.js
@@ -1,20 +1,11 @@
+// A module added to use .env file for using environmental variables like MONGODB_URI and PORT
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://asmund_olafsen:WdvQAFgn5xlZFftR@cluster0.e5hd8tb.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+// Import a Blog model from database
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
@@ -26,7 +17,21 @@ app.get('/api/blogs', (request, response) => {
       response.json(blogs)
     })
 })
-
+app.get('/api/blogs/:id', (request, response) => {
+    // Find the blog with the requested id in database
+    Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
 app.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
 
@@ -35,9 +40,30 @@ app.post('/api/blogs', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+    .catch(error => next(error))
 })
 
-const PORT = 3003
+// This middleware must be executed after all routes. If none of the above routes is processed, this middleware is called. It means that the unknown endpoint was requested. After this middleware only the error hadling middlewares must be executed.
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    // The error handler checks if the error is a CastError exception, in which case we know that the error was caused by an invalid object id for Mongo. In this situation, the error handler will send a response to the browser with the response object passed as a parameter. In all other error situations, the middleware passes the error forward to the default Express error handler.
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
