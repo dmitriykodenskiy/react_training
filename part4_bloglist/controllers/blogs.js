7b16cd4657464a7c759fb18f7c15609da528d529
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-02-07 15:14:12 +0200

    delete and update tests

--- part4_bloglist/controllers/blogs.js
+++ part4_bloglist/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const logger = require('../utils/logger')
+require('express-async-errors')
 
 // Import a Blog model from database
 const Blog = require('../models/blog')
@@ -9,30 +10,40 @@ blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs)
 })
-blogsRouter.get('/:id', (request, response) => {
-    // Find the blog with the requested id in database
-    Blog.findById(request.params.id)
-    .then(blog => {
-    if (blog) {
-        response.json(blog)
-    } else {
-        response.status(404).end()
+blogsRouter.get('/:id', async (request, response) => {
+    try {
+        // Find the blog with the requested id in database
+        const blog = await Blog.findById(request.params.id)
+        if (blog) {
+            response.json(blog)
+        } else {
+            response.status(404).end()
+        }
+    } catch (error) {
+        logger.error(error);
+        response.status(400).send({ error: 'malformatted id' })
     }
-    })
-    .catch(error => {
-    logger.error(error);
-    response.status(400).send({ error: 'malformatted id' })
-    })
 })
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
+blogsRouter.post('/', async (request, response) => {
+    const newBlog = new Blog(request.body)
+    if (!newBlog.title || !newBlog.author) {
+        return response.status(400).json({
+            error: 'content missing'
         })
-        .catch(error => next(error))
+    }
+    const savedBlog = await newBlog.save()
+    response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const updatedOption = request.body
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, updatedOption, { new: true, runValidators: true, context: 'query' })
+    response.status(200).json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- part4_bloglist/tests/blog_api.test.js
+++ part4_bloglist/tests/blog_api.test.js
@@ -1,49 +1,75 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 
 const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-const initialBLogs = [
-    {
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-    },
-    {
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10
-    }
-]
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(initialBLogs)
+    await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  const blogs = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
 
-  console.log(blogs.body);
-}, 100000)
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    const blogs = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-test('check id property', async () => {
-    const blogs = await api.get('/api/blogs')
-    blogs.body.forEach(blog => {
-        expect(blog.id).toBeDefined()
-    });
     console.log(blogs.body);
-}, 100000)
+  }, 100000)
 
-test('a valid blog can be added', async () => {
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.title)
+
+    expect(contents).toContain('First class tests')
+  })
+})
+
+describe('viewing a specific blog', () => {
+  test('succeeds with a valid id', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToView = blogsAtStart[0]
+
+    const resultBlog = await api
+      .get(`/api/blogs/${blogToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(resultBlog.body).toEqual(blogToView)
+  })
+
+  test('fails with statuscode 404 if blog does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId()
+
+    await api
+      .get(`/api/blogs/${validNonexistingId}`)
+      .expect(404)
+  })
+
+  test('fails with statuscode 400 if id is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
+
+    await api
+      .get(`/api/blogs/${invalidId}`)
+      .expect(400)
+  })
+})
+
+describe('addition of a new blog', () => {
+  test('succeeds with valid data', async () => {
     const newBlog = {
         title: "TDD harms architecture",
         author: "Robert C. Martin",
@@ -57,10 +83,81 @@ test('a valid blog can be added', async () => {
         .expect(201)
         .expect('Content-Type', /application\/json/)
 
-    const response = await api.get('/api/blogs')
+    const blogsAtEnd = await helper.blogsInDb()
 
-    expect(response.body).toHaveLength(initialBLogs.length + 1)
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map(r => r.title)
+
+    expect(contents).toContain('TDD harms architecture')
+  })
+
+  test('fails with status code 400 if data invalid', async () => {
+    const newBlog = {
+      important: true
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const contents = blogsAtEnd.map(r => r.title)
+
+    expect(contents).not.toContain(blogToDelete.title)
+  })
+})
+
+describe('editing the blog', () => {
+  test('succeeds with valid data', async () => {
+    const updatedBlog = {
+        likes: 666,
+    }
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const contents = blogsAtEnd.map(r => r.likes)
+
+    expect(contents).toContain(666)
+  })
+})
+
+test('check id property', async () => {
+    const blogs = await helper.blogsInDb()
+    blogs.body.forEach(blog => {
+        expect(blog.id).toBeDefined()
+    });
+    console.log(blogs.body);
+}, 100000)
+
 
 afterAll(async () => {
   await mongoose.connection.close()
