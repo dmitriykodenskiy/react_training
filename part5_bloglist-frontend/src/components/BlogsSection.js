6889152f94d934b8eb2dfce750e4f0b800b6082e
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-02-12 16:49:34 +0200

    sort blogs by likes

--- part5_bloglist-frontend/src/components/BlogsSection.js
+++ part5_bloglist-frontend/src/components/BlogsSection.js
@@ -1,9 +1,10 @@
 import Blog from '../components/Blog'
 
 const BlogsSection = ({ blogs, addLike }) => {
+    const sortedBlogs = blogs.sort((a, b) =>  a.likes === b.likes ? 0 : a.likes < b.likes ? 1 : -1)
     return(
         <div className='blogs_list'>
-            {blogs.map(blog =>
+            {sortedBlogs.map(blog =>
                 <Blog key={blog.id} blog={blog} addLike={addLike}/>
             )}
         </div>
