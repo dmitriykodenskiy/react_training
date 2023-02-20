19e9031d5094e1f9baec67528e36d4d0ba54e9e0
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-02-20 18:05:55 +0200

    get anecdotes from backend

--- part6_redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6_redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,18 +1,23 @@
-import initialState from '../data/initialState'
+// import initialState from '../data/initialState'
 import { createSlice } from '@reduxjs/toolkit'
 
 const anecdoteSlice = createSlice({
   name: 'anecdote',
-  initialState: initialState,
+  initialState: [],
   reducers: {
     increaseLikes(state, action) {
+      if (state.length === 0) return state
       return state.map(item => item.id === action.payload ? {...item, votes: item.votes + 1} : item)
     },
     addAnecdote(state, action) {
+      if (state.length === 0) return state
       return [...state, action.payload]
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { increaseLikes, addAnecdote } = anecdoteSlice.actions
+export const { increaseLikes, addAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
