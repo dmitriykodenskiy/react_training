abf8093f6a18a0580700b799a944f12c03ccdc58
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-02-20 18:13:05 +0200

    send data to the backend

--- part6_redux-anecdotes/src/components/AnecdoteForm.js
+++ part6_redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { addNotification, removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const generateId = (idsArray) => {
     const id = (10000 * Math.random() + 1).toFixed(0)
@@ -14,11 +15,13 @@ const generateId = (idsArray) => {
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
     const anecdoteIds = useSelector(state => state.anecdotes.map(anecdote => anecdote.id))
-    const addNew = (event) => {
+    const addNew = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(addAnecdote({content, id: generateId(anecdoteIds), votes: 0}))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(addAnecdote(newAnecdote))
+        // dispatch(addAnecdote({content, id: generateId(anecdoteIds), votes: 0}))
         dispatch(addNotification(`You've added "${content}"`))
         setTimeout(() => {
             dispatch(removeNotification())
