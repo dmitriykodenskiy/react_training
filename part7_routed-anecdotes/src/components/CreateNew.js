77abb5aeee63690040ef0711538c8f067e8b525b
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-02-24 12:10:22 +0200

    create custom useField hook

--- part7_routed-anecdotes/src/components/CreateNew.js
+++ part7_routed-anecdotes/src/components/CreateNew.js
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('content')
+    const author = useField('author')
+    const info = useField('info')
   
   
+    const handleReset = () => {
+      content.reset()
+      author.reset()
+      info.reset()
+    }
     const handleSubmit = (e) => {
       e.preventDefault()
       props.addNew({
-        content,
-        author,
-        info,
+        content: content.value,
+        author: author.value,
+        info: info.value,
         votes: 0
       })
     }
@@ -22,17 +27,18 @@ const CreateNew = (props) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input {...content} />
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input {...author} />
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input {...info} />
           </div>
-          <button>create</button>
+          <button type="submit">create</button>
+          <button type="reset" onClick={handleReset}>reset</button>
         </form>
       </div>
     )  
