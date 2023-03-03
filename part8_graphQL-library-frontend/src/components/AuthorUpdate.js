89659920b9154235100f7730aa3e3aaba5251771
Dmytro Kodenskyi <dmytro.kodenskyi@valtech.com>
2023-03-03 13:33:47 +0200

    add authors select

--- part8_graphQL-library-frontend/src/components/AuthorUpdate.js
+++ part8_graphQL-library-frontend/src/components/AuthorUpdate.js
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import { SET_BORN_DATE, ALL_AUTHORS } from "../queries"
-import { useMutation } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
+import Select from 'react-select';
+
 
 const AuthorUpdate = (props) => {
   const [bornDate, setbornDate] = useState('')
   const [name, setName] = useState('')
-
+  const result = useQuery(ALL_AUTHORS)
   const [ createBook ] = useMutation(SET_BORN_DATE, {
     refetchQueries: [ {query: ALL_AUTHORS} ]
+  })
+
+  if (result.loading) {
+    return <div>loading...</div>
+  } else if(!result.data?.allAuthors){
+    return <div>No authors found</div>
+  }
+
+  let authors = [...result.data.allAuthors]
+
+  const options = authors.map(item => {
+    return {value: item.name, label: item.name}
   })
 
   const submit = async (event) => {
@@ -19,14 +33,18 @@ const AuthorUpdate = (props) => {
     setName('')
   }
 
+  const handleSelect = ({value}) => {
+    setName(value)
+  }
   return (
     <div>
       <form onSubmit={submit}>
         <div>
           Author Name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
+          <Select
+            defaultValue={name}
+            onChange={handleSelect}
+            options={options}
           />
         </div>
         <div>
